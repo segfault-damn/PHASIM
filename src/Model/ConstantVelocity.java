d94a61f8582170673c91e4d3e4c4e2431b5b8a84
wangkj20 <wangkaijie2000@yeah.net>
2021-03-05 19:26:46 -0800

    First version (Newton's 2th law)

--- src/Model/ConstantVelocity.java
+++ src/Model/ConstantVelocity.java
@@ -170,7 +170,7 @@ public class ConstantVelocity extends JPanel implements ActionListener {
         resumeB.setBackground(new Color(171, 234, 253,30));
     }
 
-    //set Restart
+    //set Restartk
     public void setRestartButton() {
         restartB = new JButton(RestartIcon);
         restartB.addActionListener(this);
